ad95bee4cc9f396ab4708229a48bb81d90169425
Nirmit Maheshwari <24971496+mfsi-nirmitm@users.noreply.github.com>
2021-06-20 19:22:01 +0530

    enable swagger

--- src/main/java/com/rollingstone/UdemyRsmortgagePersonServiceNewApplication.java
+++ src/main/java/com/rollingstone/UdemyRsmortgagePersonServiceNewApplication.java
@@ -3,10 +3,11 @@ package com.rollingstone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableAutoConfiguration // Sprint Boot Auto Configuration
 @ComponentScan(basePackages = "com.rollingstone")
-
+@EnableSwagger2
 public class UdemyRsmortgagePersonServiceNewApplication {
 
 	public static void main(String[] args) {
